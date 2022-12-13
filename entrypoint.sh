6d31bd833e6a2ee5f0f43c504814d3d78a31982b
Vo Ngoc Trieu <trieugia008@gmail.com>
2022-12-14 02:30:23 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,7 @@ chmod 600 "$SSH_PATH/deploy_key"
 GIT_COMMAND="git push dokku@$HOST:$PROJECT"
 
 echo "Detect the project default branch: master or main"
+DEFAULT_BRANCH="$(git config --global --add safe.directory /github/workspace)"
 DEFAULT_BRANCH="$(git remote show origin | awk '/HEAD branch/ {print $NF}')"
 
 if [ -z "$DEFAULT_BRANCH" ]
